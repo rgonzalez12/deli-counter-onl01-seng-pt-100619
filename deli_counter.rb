d9e79bc19bf4d6de9851d8aece43872022b3ff23
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-10-14 05:52:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  
+  if array == []
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+  
+end
+
+def now_serving(array)
+  
+end
